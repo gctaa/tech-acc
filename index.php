21b400991c4c36495a16212d2ae88f5f1de09ecb
jgrohmann <jgrohmann1@gmail.com>
2016-06-14 14:30:01 +0000

    Colored things

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ window.onload = window.onresize = function () {
 <!-- This is the php that includes the header -->
 <?php include("header.inc"); ?>
 <main>
-ATIC Home
+Arlngton Tech - Arlington Career Center
 <h1 class="pageTitle">Home</h1>
 <!-- Page title above. <hr> is the weird break black line below. Never knew about it till now -->
 <hr>
@@ -59,7 +59,7 @@ ATIC Home
 </div>
 
 <!-- This is a discription of the website underneath the slideshow -->
-<p>Welcome to the Olorun project's official website. This website will serve as a way To show the project's progress and triumphs.</p> 
+<p>Welcome to the Arlington Tech website, on this site you will be able to find important information regarding the Arlington Tech programs, projects, news.</p> 
 </section>
 
 
@@ -68,18 +68,18 @@ ATIC Home
 <!-- These are the boxes that will describe the diffrent sections of the websites and links to them -->
 <div class="boxes">
   <a class="setbox" href="about.php">
-    <h2>About olorun</h2>
-    <p>Learn more about the Olorun project</p>
+    <h2>About the program</h2>
+    <p>Picture maybe?</p>
   </a>
   <a class="setbox" href="/project">
-    <h2>What are we doing?</h2>
-    <p>Learn more about the Olorun project is currently involved in</p>
+    <h2>History/Legacy of the program</h2>
+    <p>Quick explanation of recent history or legacy</p>
   </a>
   </div>
 <div class="boxes">
   <a class="setbox" href="info.php">
-    <h2>info</h2>
-    <p>information about our projects</p>
+    <h2>Info</h2>
+    <p>information for students</p>
   </a>
   <a class="setbox" href="/contact">
     <h2>Contact us</h2>
