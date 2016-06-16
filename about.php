a8f59f59e87ad7c9d3d899ac05a56d408bf9229f
James Johnson <tripp.johnson4934@gmail.com>
2016-06-16 18:29:17 +0000

    garbage day

--- about.php
+++ about.php
@@ -1,42 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Olorun About</title>
-
-<link rel="icon" href="static/media/OlorunSM.png">
-<link rel="stylesheet" href="static/css/body.css">
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
-</script>
-<script>
-$(document).ready(function() {
-  $('div#navb').click(function() {
-    $('nav#mainNav').addClass('mobileNav');
-    $('header div#navb').addClass('hide');
-    $('header div#close').removeClass('hide');
-  });
-  $('header div#close').click(function() {
-    $('nav#mainNav').removeClass('mobileNav');
-    $('header div#navb').removeClass('hide');
-    $('header div#close').addClass('hide');
-  });
-});
-</script>
-<script type="text/javascript">
-// Written by Kevin Cole [kjcole@ubuntu.com] 2014.10.24
-//
-// Dynamically resize the slideshow parent div to the size of the
-// auto-resizing images within it. Sadly, it appears I cannot
-// force the slideshow parent div to resize any other way. Bugger.
-
-window.onload = window.onresize = function () {
-  var slides = document.getElementById("slides");
-  var slide  = slides.children[0];
-  slides.style.height = slide.offsetHeight + "px";
-}
-</script>
-</head>
-
 <body>
 <!-- This is the php that includes the header -->
 <?php include("header.inc"); ?>
