ffed52f78abff21115010d08b7b31e72a6da6efc
James Johnson <tripp.johnson4934@gmail.com>
2016-06-17 14:58:25 +0000

    new FAQ

--- about.php
+++ about.php
@@ -1,35 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include("header.inc"); ?>
-<body>
 <!-- This is the php that includes the header -->
 
 
 <main>
 
 <!-- These sections can be repeated as many times as nessisary to organize information(15yy here to copy) -->
-<section class="info">
+<div class="info">
 
 
 <!-- This is the main picture for the page -->
-<img class="left" src="static/media/projectImages/acc_sign.jpg"/>
+<img class="left" src="static/media/projectImages/acc_sign.jpg" alt="picture"/>
 
 <!-- This is a discription of the website underneath the picture -->
 <p class="right">Project Olorun is a multidisciplinary effort to create a solar recharge station to help provide electricity to  communities that are too remote to facilitate a hardwired solution. </p>
 
 <!-- This is the black line that is at the bottom --> 
-</section>
+</div>
 <hr>
-<section class="info">
+<div class="info">
 <!-- This is the second picture for the page -->
 
-<img class="right" src="static/media/projectImages/acc_sign.jpg"/>
+<img class="right" src="static/media/projectImages/acc_sign.jpg" alt="picture"/>
 
 <!-- This is the second discription of the website underneath the picture -->
 <p class="left"> some of the diciplines included are: carpentry, electrical engineering, computer science and mathamatics  </p>
 
 <!-- This is the second black line that is at the bottom --> 
-</section>
+</div>
 <hr>
 </main>
 
